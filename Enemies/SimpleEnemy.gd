15e2217165dbf67c2e7a254147aba057ebe49761
Ciaran Woodward <ciaranwoodward@gmail.com>
2021-04-24 13:20:28 +0100

    start working on enemy with animation-driven logic

---
+++ Enemies/SimpleEnemy.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+export var fall_velocity = 0.0
+
+onready var animTree = $AnimationTree
+onready var stateMachine : AnimationNodeStateMachinePlayback = animTree["parameters/StateMachine/playback"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	stateMachine.start("Idle")
+
+func _handle_falling(delta):
+	if is_on_floor():
+		fall_velocity = 0
+		if stateMachine.get_current_node() == "Falling":
+			stateMachine.travel("Idle")
+	else:
+		stateMachine.travel("Falling")
+
+func _physics_process(delta):
+	_handle_falling(delta)
+	move_and_collide(Vector2(0, fall_velocity) * delta)
