2c96f7716263f458a31367b2b1b8747318228353
Ciaran Woodward <ciaranwoodward@gmail.com>
2021-04-27 01:05:10 +0100

    Begin hooking up levels

---
+++ SplashScreens/SplashScreen.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+export var dest_scene : PackedScene = null
+
+var minimum_timer : Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	minimum_timer = Timer.new()
+	add_child(minimum_timer)
+	minimum_timer.start(2)
+	minimum_timer.one_shot = true
+
+func _input(event: InputEvent) -> void:
+	if !minimum_timer.is_stopped():
+		return
+	if event is InputEventKey || event is InputEventJoypadButton:
+		if is_instance_valid(dest_scene):
+			get_tree().change_scene_to(dest_scene)
